e99f60c938a3269b68607927059094634bd1796e
frenkiart <frenki_art@mail.ru>
2022-09-05 21:31:54 +0300

    feat: add clickable and changable elements

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState("test text");
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
+  function inputHandler(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +26,40 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>хе-хе</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
       </header>
+
+      <main>
+        <h2>{likes}</h2>
+
+        <p>
+          <button onClick={increment} className="btn-1">
+            Increment
+          </button>
+          <button onClick={decrement} className="btn-1">
+            Decrement
+          </button>
+        </p>
+
+        <h2>{value}</h2>
+        <p>
+          <input
+            type="text"
+            onChange={inputHandler}
+            defaultValue={value}
+            className="input"
+          ></input>
+        </p>
+      </main>
     </div>
   );
 }
