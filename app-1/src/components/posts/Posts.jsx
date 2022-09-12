b61b3d54be55e9789cfb8a11cbbad9419e42f50f
frenkiart <frenki_art@mail.ru>
2022-09-12 21:57:13 +0300

    feat: add transition library

--- app-1/src/components/posts/Posts.jsx
+++ app-1/src/components/posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Post from "../post/Post";
 import "./posts.scss";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const Posts = (props) => {
   if (!props.posts.length) {
@@ -15,16 +16,20 @@ const Posts = (props) => {
     <div className="posts">
       <div className="posts__title">{props.title || "Список постов"}</div>
 
-      {props.posts.map((post, index) => {
-        return (
-          <Post
-            removePost={props.removePost}
-            number={index + 1}
-            post={post}
-            key={post.id}
-          />
-        );
-      })}
+      <TransitionGroup>
+        {props.posts.map((post, index) => {
+          return (
+            <CSSTransition key={post.id} timeout={500} classNames="wrap-post">
+              <Post
+                removePost={props.removePost}
+                number={index + 1}
+                post={post}
+                key={post.id}
+              />
+            </CSSTransition>
+          );
+        })}
+      </TransitionGroup>
     </div>
   );
 };
