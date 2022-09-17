c3fcb37b1eeb5588ae160e28805258ad4038dadc
frenkiart <frenki_art@mail.ru>
2022-09-17 20:06:34 +0300

    update

--- skandiloveka/src/pages/about.js
+++ skandiloveka/src/pages/about.js
@@ -37,6 +37,33 @@ const AboutPage = () => (
 
           <h3>Мы на карте</h3>
 
+          {isClient ? (
+            <div style="position:relative;overflow:hidden;">
+              <a
+                href="https://yandex.ru/maps/org/skandiloveka/178421107761/?utm_medium=mapframe&utm_source=maps"
+                style="color:#eee;font-size:12px;position:absolute;top:0px;"
+              >
+                SkandiLoveKa
+              </a>
+              <a
+                href="https://yandex.ru/maps/11119/republic-of-tatarstan/category/cafe/184106390/?utm_medium=mapframe&utm_source=maps"
+                style="color:#eee;font-size:12px;position:absolute;top:14px;"
+              >
+                Кафе в Республике Татарстан
+              </a>
+              <iframe
+                src="https://yandex.ru/map-widget/v1/-/CCUVRXGYsC"
+                width="560"
+                height="400"
+                frameborder="1"
+                allowfullscreen="true"
+                style="position:relative;"
+              ></iframe>
+            </div>
+          ) : (
+            ""
+          )}
+
           {isClient && (
             <div style="position:relative;overflow:hidden;">
               <a
